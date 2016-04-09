75cfeff02aeee6ef648563063e5a3d59b443c256
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-04-09 18:22:51 +0200

    added photo to index content, added icons on footer and tweaked general styling on page

--- src/scripts/_navigation.js
+++ src/scripts/_navigation.js
@@ -5,11 +5,14 @@
 	var $mob_menu = $($navbar).find('.mob-menu');
 	var $menu = $($navbar).find('ul');
 	var $close = $($navbar).find('.close');
+	var $logo = $($navbar).find('#logo');
 
 	//Bind events
 	$($mob_menu).on('click', toggleNav);
 
 	$($close).on('click', toggleNav);
+
+	$(window).on('scroll', stickyNav);
 
 	// shows/hides navigation menu on mobile
 	function toggleNav() {
@@ -17,13 +20,32 @@
 		$($menu).stop().delay(100).slideToggle(1000, 'easeOutElastic');
 	};
 
-	//ensures menu is shown when window is resized
-	$(window).resize(function() {
-		if ($(window).width() >= 750) {
-			$($menu).show();
+	//makes navbar stick to top on scroll
+	function stickyNav() {
+		var y_offset = window.pageYOffset;
+
+        if (y_offset > 50) {
+			// $($menu).css({'position':'fixed', 'top': '0'});
+			$($logo).addClass('small');
 		}
 		else {
-			$($menu).hide();
+			// $($menu).css('position', 'initial');
+			$($logo).removeClass('small');
+		}
+	};
+
+	//ensures menu is shown when window is resized
+	var vw = $(window).width();
+
+	$(window).resize(function() {
+		if ($(window).width !== vw) {
+			$($close).hide();
+			if ($(window).width() >= 750) {
+				$($menu).show();
+			}
+			else {
+				$($menu).hide();
+			}
 		}
 	});
 
