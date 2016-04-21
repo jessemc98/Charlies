7515bece5a90a9f45d33d35319ba266f5a082446
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-04-21 23:43:19 +0200

    added page for jbeauty with tabular menu (missing content)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,8 @@
 // IMPORT NAVIGATION //
 var _jqueryEasing = require('./_jqueryEasing');
 var _navigation = require('./_navigation');
+var easytabs = require('./jquery.easytabs');
 
 _jqueryEasing();
 _navigation();
+easytabs();
